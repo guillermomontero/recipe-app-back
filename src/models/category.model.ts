34a4397ad9dec4d1294724ae03bc973c44def581
Guillermo Montero <gmontero@imaster.golf>
2023-07-27 17:02:58 +0200

    WIP add sequence to categories

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,4 +1,18 @@
-import { prop, getModelForClass } from '@typegoose/typegoose';
+import { pre, prop, getModelForClass } from '@typegoose/typegoose';
+import SequenceModel from './sequence.model';
+
+@pre<Category>('save', async function (next) {
+  const doc = this as Category;
+  const sequence = await SequenceModel.findOneAndUpdate(
+    { name: 'categories' },
+    { $inc: { value: 1 } },
+    { new: true, upsert: true }
+  );
+
+  doc.value = sequence.value;
+
+  next();
+})
 
 export class Category {
   @prop({ default: '', trim: true })
